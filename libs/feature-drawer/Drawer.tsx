40c2fade253d36d9eabc419f3ccb1bca7f4e258f
규훈 <jade@codeit.com>
2022-05-18 11:35:30 +0900

    사이즈 기본값 추가

--- libs/feature-drawer/Drawer.tsx
+++ libs/feature-drawer/Drawer.tsx
@@ -1,20 +1,18 @@
 import {
-  Button,
-  Container,
-  Divider,
-  IconButton,
-  Stack,
-  SwipeableDrawer,
-  ToggleButton,
-  ToggleButtonGroup,
+  Button, Container, Divider, IconButton, Stack,
+  SwipeableDrawer, ToggleButton, ToggleButtonGroup,
   Typography,
 } from '@mui/material'
 import Grid from '@mui/material/Grid'
-import { AddCircle, RemoveCircle } from '@mui/icons-material'
+import {
+  AddCircle, RemoveCircle,
+} from '@mui/icons-material'
 import { useEffect, useMemo, useState } from 'react'
 import { Billboard } from '~/libs/feature-billboard'
 import { useStore } from '~/libs/feature-store'
-import { Size, Temperature } from '~/libs/feature-menu/model'
+import {
+  Size, Temperature,
+} from '~/libs/feature-menu/model'
 import { currency } from '~/libs/util'
 
 interface stateProps {
@@ -26,7 +24,7 @@ interface stateProps {
 interface DrawerProps {
   open: boolean
   defaultTemperature?: Temperature
-  defaultSize?: Size | ''
+  defaultSize?: Size
   defaultCount?: number
   buttonLabelLeft: string
   buttonLabelRight: string
@@ -39,7 +37,7 @@ interface DrawerProps {
 export const Drawer = ({
   open,
   defaultTemperature = 'hot',
-  defaultSize = '',
+  defaultSize = 'tall',
   defaultCount = 1,
   buttonLabelLeft,
   buttonLabelRight,
@@ -49,29 +47,29 @@ export const Drawer = ({
   onClose,
 }: DrawerProps) => {
   const { selectedMenuAmount } = useStore()
-  const [temperature, setTemperature] =
+  const [ temperature, setTemperature ] =
     useState<Temperature>(defaultTemperature)
-  const [size, setSize] = useState<Size | ''>(defaultSize)
-  const [count, setCount] = useState<number>(defaultCount)
+  const [ size, setSize ] = useState<Size | ''>(defaultSize)
+  const [ count, setCount ] = useState<number>(defaultCount)
 
   useEffect(() => {
     setTemperature(defaultTemperature)
     setSize(defaultSize)
     setCount(defaultCount)
-  }, [defaultTemperature, defaultSize, defaultCount])
+  }, [ defaultTemperature, defaultSize, defaultCount ])
 
   const selectableSizes: string[] = useMemo(() => {
     return Object.keys(selectedMenuAmount).filter(
-      (size) => selectedMenuAmount[size] > 0
+      (size) => selectedMenuAmount[ size ] > 0,
     )
-  }, [selectedMenuAmount])
+  }, [ selectedMenuAmount ])
 
   const resultAmount = useMemo(() => {
     if (size === '') {
       return 0
     }
-    return selectedMenuAmount[size] * count
-  }, [size, selectedMenuAmount, count])
+    return selectedMenuAmount[ size ] * count
+  }, [ size, selectedMenuAmount, count ])
 
   const handleTemperatureChange = (_, value) => {
     if (value) setTemperature(value)
@@ -117,29 +115,29 @@ export const Drawer = ({
   return (
     <SwipeableDrawer
       anchor="bottom"
-      open={open}
-      onClose={handleClose}
-      onOpen={onOpen}
+      open={ open }
+      onClose={ handleClose }
+      onOpen={ onOpen }
     >
-      <Container sx={{ padding: 2 }}>
+      <Container sx={ { padding: 2 } }>
         <Billboard />
         <Divider
-          sx={{
+          sx={ {
             margin: 1,
             borderStyle: 'dashed',
-          }}
+          } }
         />
-        <Stack spacing={2}>
+        <Stack spacing={ 2 }>
           <ToggleButtonGroup
             color="primary"
-            value={temperature}
+            value={ temperature }
             exclusive
             fullWidth
-            sx={{
+            sx={ {
               width: '50%',
               margin: '0 auto',
-            }}
-            onChange={handleTemperatureChange}
+            } }
+            onChange={ handleTemperatureChange }
           >
             <ToggleButton value="hot" color="error">
               HOT
@@ -147,32 +145,32 @@ export const Drawer = ({
             <ToggleButton value="ice">ICE</ToggleButton>
           </ToggleButtonGroup>
         </Stack>
-        <Typography variant="subtitle1" mt={1}>
+        <Typography variant="subtitle1" mt={ 1 }>
           사이즈
         </Typography>
-        <Stack mt={1} spacing={2}>
+        <Stack mt={ 1 } spacing={ 2 }>
           <ToggleButtonGroup
             color="standard"
-            value={size}
+            value={ size }
             exclusive
             fullWidth
-            onChange={handleSizeChange}
+            onChange={ handleSizeChange }
           >
-            {selectableSizes.map((size) => (
-              <ToggleButton key={size} value={size}>
-                {size}
+            { selectableSizes.map((size) => (
+              <ToggleButton key={ size } value={ size }>
+                { size }
               </ToggleButton>
-            ))}
+            )) }
           </ToggleButtonGroup>
         </Stack>
         <Grid
           container
           direction="row"
           justifyContent="space-between"
-          mt={2}
+          mt={ 2 }
           alignItems="center"
         >
-          <Grid item sx={{ width: '35%' }}>
+          <Grid item sx={ { width: '35%' } }>
             <Grid
               container
               direction="row"
@@ -182,48 +180,52 @@ export const Drawer = ({
               <IconButton
                 aria-label="minus"
                 component="span"
-                disabled={count === 1}
-                sx={{
+                disabled={ count === 1 }
+                sx={ {
                   color: 'black',
-                }}
-                onClick={subtractCount}
+                } }
+                onClick={ subtractCount }
               >
                 <RemoveCircle />
               </IconButton>
-              <Typography variant="h6">{count}</Typography>
+              <Typography variant="h6">{ count }</Typography>
               <IconButton
                 aria-label="plus"
                 component="span"
-                sx={{
+                sx={ {
                   color: 'black',
-                }}
-                onClick={addCount}
+                } }
+                onClick={ addCount }
               >
                 <AddCircle />
               </IconButton>
             </Grid>
           </Grid>
           <Grid item>
-            <Typography variant="h6">{currency(resultAmount)} 원</Typography>
+            <Typography variant="h6">{ currency(resultAmount) } 원</Typography>
           </Grid>
         </Grid>
-        <Grid container justifyContent="space-evenly" mt={2}>
+        <Grid
+          container
+          justifyContent="space-evenly"
+          mt={ 2 }
+        >
           <Grid item>
             <Button
-              disabled={size === ''}
+              disabled={ size === '' }
               variant="contained"
-              onClick={handleClickLeft}
+              onClick={ handleClickLeft }
             >
-              {buttonLabelLeft}
+              { buttonLabelLeft }
             </Button>
           </Grid>
           <Grid item>
             <Button
-              disabled={size === ''}
+              disabled={ size === '' }
               variant="outlined"
-              onClick={handleClickRight}
+              onClick={ handleClickRight }
             >
-              {buttonLabelRight}
+              { buttonLabelRight }
             </Button>
           </Grid>
         </Grid>
