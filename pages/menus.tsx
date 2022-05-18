5ad9ae5707b91a3b1461126ee0bd02e0f8446e3b
규훈 <jade@codeit.com>
2022-05-18 14:52:16 +0900

    빌보드 클릭시 카트로 진입

--- pages/menus.tsx
+++ pages/menus.tsx
@@ -12,69 +12,79 @@ import { useStore } from '~/libs/feature-store'
 import { Amount } from '~/libs/feature-menu/model'
 import { Floating } from '~/libs/ui-floating'
 import { useSearch } from '~/libs/feature-search'
+import Link from 'next/link'
 
 export default function Menus() {
-  const { menus, couponAmount, setSelectedMenuAmount, addCart } = useStore()
-  const [selectedMenuId, setSelectedMenuId] = useState<number | undefined>()
-  const { filteredMenus, debouncedHandleChange } = useSearch(menus)
+  const {
+    menus, couponAmount, setSelectedMenuAmount, addCart,
+  } = useStore()
+  const [ selectedMenuId, setSelectedMenuId ] = useState<number | undefined>()
+  const {
+    filteredMenus, debouncedHandleChange,
+  } = useSearch(menus)
 
   let router = useRouter()
   useEffect(() => {
     if (couponAmount === 0) {
       router.replace('/')
     }
-  }, [couponAmount, router])
+  }, [ couponAmount, router ])
 
-  const [drawerOpen, setDrawerOpen] = useState(false)
+  const [ drawerOpen, setDrawerOpen ] = useState(false)
 
-  const handleListItemClick = (id: number, amount: Amount) => {
+  const handleListItemClick = (id: number,
+    amount: Amount) => {
     setSelectedMenuId(id)
     setSelectedMenuAmount(amount)
     setDrawerOpen(true)
   }
 
   return (
-    <Layout loading={!couponAmount}>
+    <Layout loading={ !couponAmount }>
       <Floating>
-        <Billboard />
+        <Link href={ '/cart' } passHref>
+          <a>
+            <Billboard />
+          </a>
+        </Link>
       </Floating>
-      <Grid container mt={2}>
+      <Grid container mt={ 2 }>
         <SearchWrapper>
-          <Search onChange={debouncedHandleChange} />
+          <Search onChange={ debouncedHandleChange } />
         </SearchWrapper>
-        <Grid item xs={12}>
+        <Grid item xs={ 12 }>
           <List>
-            {filteredMenus.map((menu) => (
+            { filteredMenus.map((menu) => (
               <MenuItem
-                key={menu.id}
-                image={menu.image}
-                name={menu.name}
-                amount={getMinAmount(menu.amount)}
-                onClick={() => handleListItemClick(menu.id, menu.amount)}
+                key={ menu.id }
+                image={ menu.image }
+                name={ menu.name }
+                amount={ getMinAmount(menu.amount) }
+                onClick={ () => handleListItemClick(menu.id, menu.amount) }
               />
-            ))}
+            )) }
           </List>
         </Grid>
       </Grid>
       <Drawer
-        open={drawerOpen}
-        buttonLabelLeft={'담고 메뉴 더 보기'}
-        buttonLabelRight={'담고 장바구니 가기'}
-        onClickLeft={(props) => {
+        open={ drawerOpen }
+        buttonLabelLeft={ '담고 메뉴 더 보기' }
+        buttonLabelRight={ '담고 장바구니 가기' }
+        onClickLeft={ (props) => {
           addCart({
             menuId: selectedMenuId,
             ...props,
           })
-        }}
-        onClickRight={(props) => {
+        } }
+        onClickRight={ (props) => {
           addCart({
             menuId: selectedMenuId,
             ...props,
           })
           router.push('/cart')
-        }}
-        onClose={() => setDrawerOpen(false)}
-        onOpen={() => setDrawerOpen(true)}
+        } }
+        onClose={ () => setDrawerOpen(false) }
+        onOpen={ () => setDrawerOpen(true) }
       />
     </Layout>
   )
